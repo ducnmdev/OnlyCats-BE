d5afd23a9d263e9b48d04f14e6d0be7c31d928ea
ducnm168 <ductiny2003@gmail.com>
2026-02-01 05:57:57 +0700

    add schema prisma

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import routes from './routes/index.js';
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true })) // form - parse data từ form -> obj
+app.use(express.json())                         // fetch - parse json -> obj
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true,
@@ -14,5 +16,5 @@ routes(app)
 
 
 app.listen(5001, () => {
-    console.log('server is running on port 5001')
+  console.log('server is running on port 5001')
 })
