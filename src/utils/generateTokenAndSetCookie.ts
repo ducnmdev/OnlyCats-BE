e7d3fd74079f5a5832694b68a62fc16cbe035fc1
ducnm168 <ductiny2003@gmail.com>
2026-03-30 05:04:27 +0700

    final commit

--- src/utils/generateTokenAndSetCookie.ts
+++ src/utils/generateTokenAndSetCookie.ts
@@ -11,7 +11,7 @@ export const generateTokenAndSetCookie = (res: Response, userId: string) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV === "production"
     })
 
